16e1589ba9ea5a7c284a6f296b6f0d3217136752
rscherrer <raphael.scherrer@evobio.eu>
2019-06-21 11:55:39 +0200

    removed stuff

--- cmake-build-debug/lib/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /cygdrive/d/CLionProjects/ExplicitGenomeSpeciation/lib/googletest-master
-# Build directory: /cygdrive/d/CLionProjects/ExplicitGenomeSpeciation/cmake-build-debug/lib/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
