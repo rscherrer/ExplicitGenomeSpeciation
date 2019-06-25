b6587e72f4d665cc748cf0dd2305413e42e7ffe9
rscherrer <raphael.scherrer@evobio.eu>
2019-06-25 20:09:55 +0200

    Arguments converted into vector of strings

--- main.cpp
+++ main.cpp
@@ -13,7 +13,10 @@
 int main(int argc, char * argv[])
 {
 
-    
+    // Convert arguments into a vector of strings
+    const std::vector<std::string> args(argv, argv + argc);
+
+    // Run the program
 
     return doMain();
 
