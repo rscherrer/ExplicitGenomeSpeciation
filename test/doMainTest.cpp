63fc48bd30304a3d235b30eaf46b9d1b1af74c55
rscherrer <raphael.scherrer@evobio.eu>
2019-06-26 11:40:52 +0200

    Main can detect invalid parameters

--- test/doMainTest.cpp
+++ test/doMainTest.cpp
@@ -22,31 +22,32 @@ BOOST_AUTO_TEST_CASE(mainCannotTakeMoreThanOneArgument)
 }
 
 
-/// Test fixture for creating a dummy parameter file
-struct createDummyParameterFile
+/// Test fixture for creating a dummy but valid parameter file
+struct createValidParameterFile
 {
 
-    createDummyParameterFile() : dummyParameterFileName("dummy_parameters.txt")
+    createValidParameterFile() : validParameterFileName("valid_parameters.txt")
     {
         BOOST_TEST_MESSAGE( "Setup fixture: dummy parameter file" );
-        dummyParameterFile.open(dummyParameterFileName);
-        dummyParameterFile << "initialPopSize 1000\nbirthRate 4.0\n";
-        dummyParameterFile.close();
+        validParameterFile.open(validParameterFileName);
+        validParameterFile << "initialPopSize 1000\nbirthRate 4.0\n";
+        validParameterFile.close();
     }
 
-    ~createDummyParameterFile()
+    ~createValidParameterFile()
     {
         BOOST_TEST_MESSAGE( "Teardown fixture: dummy parameter file" );
     }
 
-    std::ofstream dummyParameterFile;
-    std::string dummyParameterFileName;
+    std::ofstream validParameterFile;
+    std::string validParameterFileName;
 
 };
 
 
 /// Test the behavior of main in relation to input file reading
-BOOST_FIXTURE_TEST_SUITE(testMainReadingAbilities, createDummyParameterFile)
+BOOST_FIXTURE_TEST_SUITE(testMainReadingAbilities, createValidParameterFile)
+
 
     /// Test that main errors if the argument is not a valid input file name
     BOOST_AUTO_TEST_CASE(mainCannotTakeInvalidInputFileAsArgument)
@@ -54,12 +55,46 @@ BOOST_FIXTURE_TEST_SUITE(testMainReadingAbilities, createDummyParameterFile)
         BOOST_CHECK_EQUAL(doMain( { "ExplicitGenomeSpeciation", "nonsense" } ), 1);
     }
 
+
     /// Test that main can run if a valid input file name is provided
     BOOST_AUTO_TEST_CASE(mainCanTakeValidInputFileAsArgument)
     {
-        BOOST_CHECK_EQUAL(doMain( { "ExplicitGenomeSpeciation", "dummy_parameters.txt" }), 0);
+        BOOST_CHECK_EQUAL(doMain( { "ExplicitGenomeSpeciation", "valid_parameters.txt" }), 0);
     }
+
 
 BOOST_AUTO_TEST_SUITE_END()
 
 
+/// Test fixture for creating a dummy parameter file with invalid parameters in it
+struct createInvalidParameterFile
+{
+
+    createInvalidParameterFile() : invalidParameterFileName("invalid_parameters.txt")
+    {
+        BOOST_TEST_MESSAGE( "Setup fixture: dummy invalid parameter file" );
+        invalidParameterFile.open(invalidParameterFileName);
+        invalidParameterFile << "height 1000\nweight 4.0\n";
+        invalidParameterFile.close();
+    }
+
+    ~createInvalidParameterFile()
+    {
+        BOOST_TEST_MESSAGE( "Teardown fixture: dummy invalid parameter file" );
+    }
+
+    std::ofstream invalidParameterFile;
+    std::string invalidParameterFileName;
+
+};
+
+
+/// Test that main errors if invalid parameters are provided in a parameter file
+BOOST_FIXTURE_TEST_SUITE(testMainWithInvalidParameters, createInvalidParameterFile)
+
+    BOOST_AUTO_TEST_CASE(mainCanDetectInvalidParametersFromAFile)
+    {
+        BOOST_CHECK_EQUAL(doMain( { "ExplicitGenomeSpeciation", "invalid_parameters.txt" } ), 1);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
