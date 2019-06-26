df42be70fd90714f0be15a7c082d2b092a325bee
rscherrer <raphael.scherrer@evobio.eu>
2019-06-26 19:18:21 +0200

    Implemented test that genetic architecture is read from file correctly

---
+++ test/GeneticArchitectureTest.cpp
@@ -0,0 +1,30 @@
+#define BOOST_TEST_DYN_LINK
+
+#include "GeneticArchitecture.h"
+#include "TestFixtureCreateArchitectureFile.h"
+#include <boost/test/unit_test.hpp>
+
+
+/// Test to check that the genetic architecture is read properly from a file
+BOOST_FIXTURE_TEST_CASE(geneticArchitectureIsLoadedProperly, createValidArchitectureFile)
+{
+
+    GeneticArchitecture geneticArchitecture;
+    parameters.architectureFileName = validArchitectureFileName;
+    geneticArchitecture.loadGeneticArchitecture(parameters);
+
+    // Check that the values are read in correctly
+    BOOST_CHECK_EQUAL_COLLECTIONS(
+            geneticArchitecture.chromosomeSizes.begin(),
+            geneticArchitecture.chromosomeSizes.end(),
+            expectedChromosomeSizes.begin(),
+            expectedChromosomeSizes.end()
+            );
+    BOOST_CHECK_EQUAL(geneticArchitecture.locusConstants[0u].trait, 0u);
+    BOOST_CHECK_EQUAL(geneticArchitecture.locusConstants[3u].location, 0.4);
+    BOOST_CHECK_EQUAL(geneticArchitecture.locusConstants[4u].effectSize, 0.01);
+    BOOST_CHECK_EQUAL(geneticArchitecture.locusConstants[5u].dominanceCoeff, 0.0);
+    BOOST_CHECK_EQUAL(geneticArchitecture.locusConstants[4u].neighbors.front().first, 3u);
+    BOOST_CHECK_EQUAL(geneticArchitecture.locusConstants[4u].neighbors.front().second, 0.01);
+
+}
--- test/doMainTest.cpp
+++ test/doMainTest.cpp
@@ -52,57 +52,11 @@ BOOST_FIXTURE_TEST_SUITE(testMainWithInvalidParameters, createInvalidParameterFi
 
 BOOST_AUTO_TEST_SUITE_END()
 
-// Wait, what is a genetic architecture file made of?
 
-// A text file with the following
-// The number of loci underlying the ecological, mating and neutral trait, respectively, each separated by a new line
-// After a new line, the number of epistatic interactions underlying the ecological, mating and neutral trait, respectively
-// After a new line, the number of chromosomes
-// After a new line, the sizes of each chromosome separated by tabulations
-// After a new line, for each locus separated by a new line, the index of the trait underlain by the locus, its location, its effect size and its dominance coefficient, separated by tabulations
-// After a new line, for each pair of interacting loci separated by a new line, the index of the first locus, the index of the second locus and the interaction weight, separated by tabulations
-// Note: each pair of interacting loci appears only once in the file
-
-// Number of loci for each trait
-2
-2
-2
-
-// Number of interactions for each trait
-1
-1
-1
-
-// Number of chromosomes
-1
-
-// Size of each chromosome
-1
-
-// Characteristics of each locus
-1
-2
-3
-4
-5
-6
 
 // Characteristics of each interaction
 
-/// Test fixture for creating a dummy valid genetic architecture
-struct createValidArchitectureFile
-{
 
-    createValidArchitectureFile() : validArchitectureFileName("valid_architecture.txt") {
-        validArchitectureFile.open(validArchitectureFileName);
-        validArchitectureFile <<
-    }
-    ~createValidArchitectureFile() {}
-
-    std::ofstream validArchitectureFile;
-    std::string validArchitectureFileName;
-
-};
 
 // Create a dummy invalid genetic architecture for testing purposes
 
@@ -111,4 +65,3 @@ struct createValidArchitectureFile
 // Check that doMain errors if a genetic architecture file has parameters that do not match previously set parameters
 // Check that doMain errors if the genetic architecture file does not have the right dimensions
 
-// Check that the genetic architecture is read properly
