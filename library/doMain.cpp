6d1762747aace868f8acae17d8d4438e389a121c
rscherrer <raphael.scherrer@evobio.eu>
2019-07-28 16:09:26 +0200

    new function to run the simulation

--- library/doMain.cpp
+++ library/doMain.cpp
@@ -10,6 +10,13 @@
 #include <cassert>
 
 
+/// Function to run a simulation
+void runSimulation()
+{
+
+}
+
+
 /// Program to run the main function
 int doMain(const std::vector<std::string> &args)
 {
@@ -30,6 +37,15 @@ int doMain(const std::vector<std::string> &args)
         GeneticArchitecture arch = GeneticArchitecture(pars, rnd);
 
         std::cout << "Architecture created\n";
+
+        // Create a population of individuals
+
+        // Run the simulation
+
+        // Loop through time
+        // At every generation perform the steps of the life cycle
+        // and update the population accordingly
+        runSimulation();
 
     }
     catch (const std::runtime_error &err)
