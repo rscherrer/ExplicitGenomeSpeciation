0e7587c02811e244e20c855b9b8fed0e0ac3279b
rscherrer <raphael.scherrer@evobio.eu>
2019-08-15 16:45:08 +0200

    tidy up the architecture header

--- library/GeneticArchitecture.h
+++ library/GeneticArchitecture.h
@@ -9,67 +9,19 @@
 #include <list>
 
 
-/// An edge is a pair of interacting loci
 typedef std::pair<size_t, size_t> Edge;
 
-/// A container for all constant genetic features of the species we are
-/// simulating
+/// A container for all constant genetic features
 class GeneticArchitecture {
 
 public:
 
-    /// Constructor
     GeneticArchitecture(const ParameterSet&);
 
     /// Getters
     std::vector<double> getChromosomeSizes() const { return chromosomeSizes; }
     std::vector<Network> getTraitNetworks() const { return traitNetworks; }
     Genome getGenome() const { return genome; }
-
-    /*
-    // High-level functions
-    void generateGeneticArchitecture(const ParameterSet&);
-    void loadGeneticArchitecture(const ParameterSet&);
-    void storeGeneticArchitecture(const ParameterSet&);
-
-    // Low-level functions
-
-    // Setting
-    void createRecombinationMap(const ParameterSet&);
-    void setChromosomeSizes(const size_t&); // obsolete
-    void sampleGeneLocations(const ParameterSet&);
-    void assignPhenotypes(const ParameterSet&);
-    void sampleEffectSizes(const ParameterSet&);
-    void sampleDominanceCoeff(const ParameterSet&);
-    void makeRegulatoryNetworks(const ParameterSet&);
-    void preferentialAttachmentNetwork(const size_t&, size_t&, const double&);
-    void initializeNetwork(size_t&, std::vector<size_t>&);
-    void growNetwork(const size_t&, size_t&, std::vector<size_t>&,
-const double&);
-    void sortNetwork(const size_t&, const std::vector<size_t>&);
-    void sampleInteractions(const ParameterSet&, const size_t&, const size_t&);
-// Per phenotypic character
-
-    // Reading
-    bool validateArchitecture(std::ifstream&, const ParameterSet&);
-    void loadChromosomeSizes(std::ifstream&);
-    void loadLocusConstants(std::ifstream&, const ParameterSet&);
-    void loadEpistaticInteractions(std::ifstream&);
-
-    // Writing
-    void writeChromosomeSizes(std::ofstream&);
-    void writeLocusConstants(std::ofstream&, const ParameterSet&);
-    void writeEpistaticInteractions(std::ofstream&, const ParameterSet&);
-
-    std::vector<double> getGenomicLocations(const size_t&);
-    std::vector<double> getChromosomeSizes(const size_t&);
-    std::vector<size_t> getEncodedTraits(const size_t&,
-const std::vector<size_t>&);
-    std::vector<double> getEffectSizes(const size_t&, const double&,
-const double&, const size_t&);
-    std::vector<double> getDominanceCoeffs(const size_t&, const size_t&);
-     */
-
 
 private:
 
@@ -97,6 +49,48 @@ private:
 
 };
 
+/*
+// High-level functions
+void generateGeneticArchitecture(const ParameterSet&);
+void loadGeneticArchitecture(const ParameterSet&);
+void storeGeneticArchitecture(const ParameterSet&);
 
+// Low-level functions
+
+// Setting
+void createRecombinationMap(const ParameterSet&);
+void setChromosomeSizes(const size_t&); // obsolete
+void sampleGeneLocations(const ParameterSet&);
+void assignPhenotypes(const ParameterSet&);
+void sampleEffectSizes(const ParameterSet&);
+void sampleDominanceCoeff(const ParameterSet&);
+void makeRegulatoryNetworks(const ParameterSet&);
+void preferentialAttachmentNetwork(const size_t&, size_t&, const double&);
+void initializeNetwork(size_t&, std::vector<size_t>&);
+void growNetwork(const size_t&, size_t&, std::vector<size_t>&,
+const double&);
+void sortNetwork(const size_t&, const std::vector<size_t>&);
+void sampleInteractions(const ParameterSet&, const size_t&, const size_t&);
+// Per phenotypic character
+
+// Reading
+bool validateArchitecture(std::ifstream&, const ParameterSet&);
+void loadChromosomeSizes(std::ifstream&);
+void loadLocusConstants(std::ifstream&, const ParameterSet&);
+void loadEpistaticInteractions(std::ifstream&);
+
+// Writing
+void writeChromosomeSizes(std::ofstream&);
+void writeLocusConstants(std::ofstream&, const ParameterSet&);
+void writeEpistaticInteractions(std::ofstream&, const ParameterSet&);
+
+std::vector<double> getGenomicLocations(const size_t&);
+std::vector<double> getChromosomeSizes(const size_t&);
+std::vector<size_t> getEncodedTraits(const size_t&,
+const std::vector<size_t>&);
+std::vector<double> getEffectSizes(const size_t&, const double&,
+const double&, const size_t&);
+std::vector<double> getDominanceCoeffs(const size_t&, const size_t&);
+ */
 
 #endif //EXPLICITGENOMESPECIATION_GENETICARCHITECTURE_H
