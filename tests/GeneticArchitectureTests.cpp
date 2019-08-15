48b9ea5b01ac4600442559b42289a7850ba5b7fb
rscherrer <raphael.scherrer@evobio.eu>
2019-08-15 16:47:09 +0200

    verbose for chromosome size test

--- tests/GeneticArchitectureTests.cpp
+++ tests/GeneticArchitectureTests.cpp
@@ -10,6 +10,7 @@
 // A genetic architecture created with 3 equal sized chromosomes
 BOOST_AUTO_TEST_CASE(threeEqualSizedChromosomes) {
 
+    std::cout << "Testing chromosome sizes...\n";
     std::vector<double> exp {(0.0 + 1.0) / 3.0, (1.0 + 1.0) / 3.0,
      (2.0 + 1.0) / 3.0};
 
