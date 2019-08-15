7ce08c6fe4eae3fd3a755c7016d0ba8bbd2d911e
rscherrer <raphael.scherrer@evobio.eu>
2019-08-15 13:31:30 +0200

    ind tests make use of genfixture

--- tests/IndividualTests.cpp
+++ tests/IndividualTests.cpp
@@ -1,57 +1,50 @@
 #include "library/Individual.h"
+#include "tests/GenFixture.h"
 #include <boost/test/unit_test.hpp>
 #include <iostream>
 
-// Check that a fully homogamous female will always accept identical mate
-BOOST_AUTO_TEST_CASE(checkAssortative)
-{
-    std::cout << "Testing homogamous female...\n";
-    ParameterSet pars;
-    GeneticArchitecture arch = GeneticArchitecture(pars);
-    Genome genome = arch.getGenome();
-    Individual ind = Individual(genome);
-    ind.setEcoTrait(0.0, 1.0);
-    ind.setMatePref(1.0);
-    BOOST_CHECK(ind.acceptMate(0.0, 1.0));
-}
+BOOST_FIXTURE_TEST_SUITE(indTestSuite, GenFixture)
 
-// Check that a fully heterogamous female will always reject identical mate
-BOOST_AUTO_TEST_CASE(checkDisassortative)
-{
-    std::cout << "Testing heterogamous female...\n";
-    ParameterSet pars;
-    GeneticArchitecture arch = GeneticArchitecture(pars);
-    Genome genome = arch.getGenome();
-    Individual ind = Individual(genome);
-    ind.setEcoTrait(0.0, 1.0);
-    ind.setMatePref(-1.0);
-    BOOST_CHECK(!ind.acceptMate(0.0, 1.0));
-}
+    // Check that a fully homogamous female will always accept identical mate
+    BOOST_AUTO_TEST_CASE(checkAssortative)
+    {
+        std::cout << "Testing homogamous female...\n";
+        Individual ind = Individual(genome);
+        ind.setEcoTrait(0.0, 1.0);
+        ind.setMatePref(1.0);
+        BOOST_CHECK(ind.acceptMate(0.0, 1.0));
+    }
 
-// Check that a random mating female will accept everybody
-BOOST_AUTO_TEST_CASE(checkRandomMating)
-{
-    std::cout << "Testing random mating female...\n";
-    ParameterSet pars;
-    GeneticArchitecture arch = GeneticArchitecture(pars);
-    Genome genome = arch.getGenome();
-    Individual ind = Individual(genome);
-    ind.setEcoTrait(0.0, 1.0);
-    ind.setMatePref(0.0);
-    BOOST_CHECK(ind.acceptMate(0.0, 1.0));
-    BOOST_CHECK(ind.acceptMate(1.0, 1.0));
-    BOOST_CHECK(ind.acceptMate(-1.0, 1.0));
-}
+    // Check that a fully heterogamous female will always reject identical mate
+    BOOST_AUTO_TEST_CASE(checkDisassortative)
+    {
+        std::cout << "Testing heterogamous female...\n";
+        Individual ind = Individual(genome);
+        ind.setEcoTrait(0.0, 1.0);
+        ind.setMatePref(-1.0);
+        BOOST_CHECK(!ind.acceptMate(0.0, 1.0));
+    }
 
-// We know exactly the fitness that a default individual should get from food
-BOOST_AUTO_TEST_CASE(checkFeeding)
-{
-    std::cout << "Testing fitness obtained by feeding...\n";
-    ParameterSet pars;
-    GeneticArchitecture arch = GeneticArchitecture(pars);
-    Genome genome = arch.getGenome();
-    Individual ind = Individual(genome);
-    ind.setEcoTrait(-1.0, 1.0);
-    ind.feed({ 100.0, 100.0 });
-    BOOST_CHECK(ind.getFitness() == 0.0004 * 100.0 + 0.0004 * exp(-4.0) * 100);
-}
+    // Check that a random mating female will accept everybody
+    BOOST_AUTO_TEST_CASE(checkRandomMating)
+    {
+        std::cout << "Testing random mating female...\n";
+        Individual ind = Individual(genome);
+        ind.setEcoTrait(0.0, 1.0);
+        ind.setMatePref(0.0);
+        BOOST_CHECK(ind.acceptMate(0.0, 1.0));
+        BOOST_CHECK(ind.acceptMate(1.0, 1.0));
+        BOOST_CHECK(ind.acceptMate(-1.0, 1.0));
+    }
+
+    // We know exactly the fitness that a default individual should get
+    BOOST_AUTO_TEST_CASE(checkFeeding)
+    {
+        std::cout << "Testing fitness obtained by feeding...\n";
+        Individual ind = Individual(genome);
+        ind.setEcoTrait(-1.0, 1.0);
+        ind.feed({ 100.0, 100.0 });
+        BOOST_CHECK(ind.getFitness() == 0.04 + 0.0004 * exp(-4.0) * 100);
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
