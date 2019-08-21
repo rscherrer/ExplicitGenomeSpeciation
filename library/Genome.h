c610081fa0c341b2ae7b3d69335d61dc4d778b90
rscherrer <raphael.scherrer@evobio.eu>
2019-08-21 19:18:21 +0200

    aliases to genome

--- library/Genome.h
+++ library/Genome.h
@@ -3,33 +3,33 @@
 
 #include <vector>
 
-typedef std::vector<double> Vector;
-typedef std::vector<size_t> Uector;
+typedef std::vector<double> dVector;
+typedef std::vector<size_t> uVector;
 
 /// A container of constant genetic features across the genome
 struct Genome
 {
 
-    Genome(const std::vector<size_t>&, const size_t&, const size_t&,
+    Genome(const uVector&, const size_t&, const size_t&,
      const double&, const double&, const double&);
 
     size_t nloci;
-    Vector chromosomes;
+    dVector chromosomes;
 
-    Uector traits;
-    Vector locations;
-    Vector effects;
-    Vector dominances;
+    uVector traits;
+    dVector locations;
+    dVector effects;
+    dVector dominances;
 
     // A vector of locus epistatic interaction partners and weights
     // std::vector<std::vector<std::pair<size_t, double> > > interactions;
 
     // Member functions
-    Vector makeChromosomes(const size_t&);
-    Uector makeEncodedTraits(const Uector&);
-    Vector makeLocations();
-    Vector makeEffects(const double&, const double&);
-    Vector makeDominances(const double& = 1.0);
+    dVector makeChromosomes(const size_t&);
+    uVector makeEncodedTraits(const uVector&);
+    dVector makeLocations();
+    dVector makeEffects(const double&, const double&);
+    dVector makeDominances(const double& = 1.0);
 
     void setLocationsEffectSizesAndDominance(const size_t&,
      const double&, const double&);
