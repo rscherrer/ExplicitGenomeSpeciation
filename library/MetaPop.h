c6fba01c06bd3f944fcd10acb1ee8bc3df11941f
rscherrer <raphael.scherrer@evobio.eu>
2019-09-04 15:28:38 +0200

    declared class metapop

--- library/MetaPop.h
+++ library/MetaPop.h
@@ -1,5 +1,15 @@
 #ifndef EXPLICITGENOMESPECIATION_METAPOP_H
 #define EXPLICITGENOMESPECIATION_METAPOP_H
 
+class MetaPop
+{
+
+public:
+    MetaPop() {}
+    ~MetaPop() {}
+
+private:
+
+};
 
 #endif
