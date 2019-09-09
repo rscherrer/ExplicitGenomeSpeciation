1acac37d7551d5c27b73f1e46a72aed3cfcf66bd
rscherrer <raphael.scherrer@evobio.eu>
2019-09-09 14:55:38 +0200

    increasing coverage by testing all correct parameter inputs

--- tests/doMainTests.cpp
+++ tests/doMainTests.cpp
@@ -14,6 +14,48 @@ void makeValidParamFile()
     out.open("valid_paramfile_test.txt");
     if (!out.is_open())
         std::cout << "Unable to open valid parameter test file.\n";
+
+    out << "maxResourceCapacity" << '\t' << 100.0 << '\n'
+        << "maxResourceGrowth" << '\t' << 1.0 << '\n'
+        << "habitatSymmetry" << '\t' << 1.0 << '\n'
+        << "ecoSelCoeff" << '\t' << 1.0 << '\n'
+        << "initialPopSize" << '\t' << 100 << '\n'
+        << "dispersalRate" << '\t' << 1.0e-3 << '\n'
+        << "birthRate" << '\t' << 4.0 << '\n'
+        << "survivalProb" << '\t' << 0.8 << '\n'
+        << "matePreferenceStrength" << '\t' << 10.0 << '\n'
+        << "mateEvalutationCost" << '\t' << 0.01 << '\n'
+        << "nEcoLoci" << '\t' << 400 << '\n'
+        << "nMatLoci" << '\t' << 200 << '\n'
+        << "nNtrLoci" << '\t' << 400 << '\n'
+        << "nEcoEdges" << '\t' << 1000 << '\n'
+        << "nMatEdges" << '\t' << 500 << '\n'
+        << "nNtrEdges" << '\t' << 0 << '\n'
+        << "nChromosomes" << '\t' << 3 << '\n'
+        << "mutationRate" << '\t' << 1.0e-5 << '\n'
+        << "recombinationRate" << '\t' << 0.01 << '\n'
+        << "genomeLength" << '\t' << 300.0 << '\n'
+        << "freqSNP" << '\t' << 0.02 << '\n'
+        << "isFemaleHeterogamy" << '\t' << 0 << '\n'
+        << "isGeneticArchitecture" << '\t' << 0 << '\n'
+        << "architectureFileName" << '\t' << "architecture.txt" << '\n'
+        << "scaleA" << '\t' << 1.0 << '\t' << 1.0 << '\t' << 1.0 << '\n'
+        << "scaleD" << '\t' << 0.0 << '\t' << 0.0 << '\t' << 0.0 << '\n'
+        << "scaleI" << '\t' << 0.0 << '\t' << 0.0 << '\t' << 0.0 << '\n'
+        << "scaleE" << '\t' << 0.0 << '\t' << 0.0 << '\t' << 0.0 << '\n'
+        << "skewnesses" << '\t' << 1.0 << '\t' << 1.0 << '\t' << 1.0 << '\n'
+        << "effectSizeShape" << '\t' << 2.0 << '\n'
+        << "effectSizeScale" << '\t' << 1.0 << '\n'
+        << "interactionWeightShape" << '\t' << 5.0 << '\n'
+        << "interactionWeightScale" << '\t' << 1.0 << '\n'
+        << "dominanceVariance" << '\t' << 1.0 << '\n'
+        << "tBurnIn" << '\t' << 1 << '\n'
+        << "tEndSim" << '\t' << 5 << '\n'
+        << "tSave" << '\t' << 1 << '\n'
+        << "tiny" << '\t' << 1.0e-12 << '\n'
+        << "seed" << '\t' << 42 << '\n'
+        << "record" << '\t' << 1 << '\n';
+
     out.close();
 }
 
