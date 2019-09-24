957ab991253e8e4907f0a83bf61a84fe042c71e8
rscherrer <raphael.scherrer@evobio.eu>
2019-09-24 15:07:57 +0200

    added library

--- compiler.sh
+++ compiler.sh
@@ -1,2 +1,2 @@
 module load foss/2018a
-g++ main.cpp Buffer.cpp Deme.cpp doMain.cpp GenArch.cpp Individual.cpp MetaPop.cpp Network.cpp Output.cpp Param.cpp Random.cpp Utilities.cpp -o simulation
+g++ main.cpp library/Buffer.cpp library/Deme.cpp library/doMain.cpp library/GenArch.cpp library/Individual.cpp library/MetaPop.cpp library/Network.cpp library/Output.cpp library/Param.cpp library/Random.cpp library/Utilities.cpp -o simulation
