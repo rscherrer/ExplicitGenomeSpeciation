d5e853844890d9179fd8722f811042d52a7137a2
rscherrer <raphael.scherrer@evobio.eu>
2019-09-24 15:57:05 +0200

    added peregrine compiler using qmake

--- compiler.sh
+++ compiler.sh
@@ -1,2 +1,2 @@
-module load foss/2018a
-g++ main.cpp library/Buffer.cpp library/Deme.cpp library/doMain.cpp library/GenArch.cpp library/Individual.cpp library/MetaPop.cpp library/Network.cpp library/Output.cpp library/Param.cpp library/Random.cpp library/Utilities.cpp -O3 -o simulation
+module load Qt5
+qmake ./EGS.pro
