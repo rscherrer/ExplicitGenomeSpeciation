b39bdcd49b40c6098918dcf0863043d1aab85afe
rscherrer <raphael.scherrer@evobio.eu>
2019-09-24 16:07:10 +0200

    commented on what qmake exactly does

--- compiler.sh
+++ compiler.sh
@@ -1,3 +1,7 @@
+# This script compiles the EGS simulation program into an executable
+# Qt5's qmake build system is used to produce the makefiles from the project file
+# Then make calls GNU's GCC to compile
+
 module load Qt5
 qmake ./EGS.pro
 make --silent release
