ec705b823ddf32061d259675bf5b8d7990ac18a0
rscherrer <raphael.scherrer@evobio.eu>
2019-09-24 16:21:18 +0200

    spit job and launcher

---
+++ job.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#SBATCH --time=00:10:00
+#SBATCH --mem=10Gb
+#SBATCH --partition=short
+
+./EGS
--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
-#SBATCH --time=00:10:00
-#SBATCH --mem=10Gb
-#SBATCH --partition=short
-
-./EGS
+sbatch job.sh
