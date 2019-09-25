53d0c76453f7d35fbf9794f09f2275cb14a47062
rscherrer <raphael.scherrer@evobio.eu>
2019-09-25 09:07:33 +0200

    removed useless files

--- library/Genome.cpp
+++
@@ -1,6 +0,0 @@
-#include "GenArch.h"
-#include "Genome.h"
-#include "Utilities.h"
-#include <cassert>
-#include <algorithm>
-#include <iostream>
--- tests/GenomeTests.cpp
+++
@@ -1,7 +0,0 @@
-#include "library/Genome.h"
-#include "library/Utilities.h"
-#include "tests/GenFixture.h"
-#include <boost/test/unit_test.hpp>
-#include <iostream>
-
-// Ditch this
