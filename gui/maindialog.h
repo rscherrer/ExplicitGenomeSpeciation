540edc8743f4875a03c477c2c60d3f10cd489f3e
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-11 11:39:26 +0200

    First stub of GUI

--- gui/maindialog.h
+++ gui/maindialog.h
@@ -2,6 +2,7 @@
 #define MAINDIALOG_H
 
 #include <QDialog>
+#include "library/Param.h"
 
 namespace Ui {
   class MainDialog;
@@ -15,8 +16,14 @@ public:
   explicit MainDialog(QWidget *parent = nullptr);
   ~MainDialog();
 
+private slots:
+  void on_run_button_clicked();
+
 private:
   Ui::MainDialog *ui;
+
+  ///Read the parameters from the GUI
+  Param createPars();
 };
 
 #endif // MAINDIALOG_H
