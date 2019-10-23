ffb583101b46c79446690529e28b20dc726de2ca
rscherrer <raphael.scherrer@evobio.eu>
2019-10-23 11:57:36 +0200

    removed junk

--- library/Random.h
+++ library/Random.h
@@ -8,6 +8,7 @@
 namespace rnd
 {
 
+    // Probability distributions
     typedef std::discrete_distribution<size_t> discrete;
     typedef std::uniform_int_distribution<size_t> random;
     typedef std::exponential_distribution<double> exponential;
@@ -19,20 +20,8 @@ namespace rnd
     typedef std::gamma_distribution<double> gamma;
     typedef std::bernoulli_distribution bernoulli;
 
-    /// Random number generator
+    // Random number generator
     extern std::mt19937_64 rng;
-
-    /// Functions to generate random numbers
-    // bool bernoulli(const double&);
-    // size_t poisson(const double&);
-    // size_t geometric(const double&);
-    // double uniform(const double&);
-    // double normal(const double&, const double&);
-    double hnormal(const double&);
-    size_t sample(const vecDbl&);
-    double flip(const double&, const double&);
-    // double gamma(const double&, const double&);
-    double bigamma(const double&, const double&);
 
 }
 
