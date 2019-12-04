2f0241a243cc2cff9978d9e8bf35ce317adc9af8
rscherrer <raphael.scherrer@evobio.eu>
2019-12-04 17:11:29 +0100

    compile only if target folder could be created

--- cluster/build_target.sh
+++ cluster/build_target.sh
@@ -10,21 +10,23 @@ if [ "$1" != "" ]; then
 		echo "Creating target folder"		
 	fi	
 	mkdir $1
+
+	# Produce an executable (from here cannot be tested locally)
+
+	module load Qt5
+	qmake ../EGS.pro # assuming we are in the cluster folder
+	make --silent release
+
+	# Move the executable to the target folder
+
+	mv EGS $1
+
+	# Copy the protocol and the running script to the target folder
+
+	cp protocol.txt $1
+	cp run_simulations.sh $1
+	
 else
 	echo "Please provide target folder"
 fi
 
-# Produce an executable (from here cannot be tested locally)
-
-module load Qt5
-qmake ../EGS.pro # assuming we are in the cluster folder
-make --silent release
-
-# Move the executable to the target folder
-
-mv EGS $1
-
-# Copy the protocol and the running script to the target folder
-
-cp protocol.txt $1
-cp run_simulations.sh $1
