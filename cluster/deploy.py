db73ef0920d1b597331b68555572b831a5d71a78
rscherrer <raphael.scherrer@evobio.eu>
2019-12-04 18:03:09 +0100

    streamlining deployment function

--- cluster/deploy.py
+++ cluster/deploy.py
@@ -3,7 +3,6 @@
 import pytilities as pyt
 import sys
 import re
-import subprocess
 
 # This program should read a protocol file (passed as single argument)
 # Combine parameter values provided and make a folder for each of them (use combine_parameters)
@@ -12,22 +11,14 @@ import subprocess
 if len(sys.argv) == 1:
 	raise Exception("Path to protocol file must be provided.")
 
-if len(sys.argv) > 3:
+if len(sys.argv) > 2:
 	raise Exception("Too many arguments provided.")
 
 # The first argument is the path to the protocol file
-args = sys.argv[1:]
-filename = args[0]
-
-# If a second argument is provided, then this is the path to the executable
-execpath = ".."
-if len(args) > 1:
-	execpath = args[1]
-execpath = execpath + "/EGS"
+filename = sys.argv[1]
 
 joboptions = ''
 paramstring = ''
-nocluster = 0
 
 # Read protocol
 with open(filename, "rt") as f:
@@ -43,29 +34,17 @@ with open(filename, "rt") as f:
 		if re.match(r'\-', line):
 			paramstring = paramstring + re.sub('\n', '', line) + ' '
 
-		# Is there a special string saying we are not on the cluster?
-		if re.match("nocluster", line):
-			nocluster = 1
-
 paramstring = re.sub(' $', '', paramstring)
 
 # Make a template job file
 job = job = "#!/bin/bash\n"
 job = job + joboptions
-job = job + execpath + ' ' + "parameters.txt\n"
+job = job + "./EGS parameters.txt\n"
 
 # Make all parameter combinations
 params = paramstring.split(' ')
 dirnames = pyt.combine_parameters(".", params)
 
-if nocluster:
-	print("You are not on the cluster, I will not submit the jobs to SLURM.") 
-
-# Copy the job file in all directories and submit to SLURM
-for dir in dirnames:
-
-	with open(dir + "/job.sh", "w+") as f:
-		f.write(job)
-
-	if not nocluster: 
-		subprocess.run("cd " + dir + "\nsbatch ./job.sh", shell = True)
+# Save the job file in the target folder
+with open("job.sh", "w+") as f:
+	f.write(job)
