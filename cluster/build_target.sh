44d59fe8fb7dd89be9cb8be77a4cdc175ddcf981
rscherrer <raphael.scherrer@evobio.eu>
2019-12-04 17:13:41 +0100

    bit more verbose during compiling

--- cluster/build_target.sh
+++ cluster/build_target.sh
@@ -3,13 +3,21 @@
 # Make a target folder
 
 if [ "$1" != "" ]; then
+
 	if [ -d "$1" ]; then
-		echo "Overwriting target folder"
+
+		echo "Overwriting target folder..."
 		rm -rf $1		
+
 	else
-		echo "Creating target folder"		
+
+		echo "Creating target folder..."	
+
 	fi	
+
 	mkdir $1
+
+	echo "Building the program..."
 
 	# Produce an executable (from here cannot be tested locally)
 
@@ -25,8 +33,12 @@ if [ "$1" != "" ]; then
 
 	cp protocol.txt $1
 	cp run_simulations.sh $1
-	
+
+	echo "Done."
+
 else
+
 	echo "Please provide target folder"
+
 fi
 
