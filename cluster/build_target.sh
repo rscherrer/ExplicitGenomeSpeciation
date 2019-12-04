ac4807a406fe2ac9456ab2c12998b28fb3c3ccbd
rscherrer <raphael.scherrer@evobio.eu>
2019-12-04 16:06:42 +0100

    script for building the target on the cluster

--- cluster/build_target.sh
+++ cluster/build_target.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
 
+# Make a target folder
+
+if [ "$1" != "" ]; then
+	if [ -d "$1" ]; then
+		echo "Overwriting target folder"
+		rm -rf $1		
+	else
+		echo "Creating target folder"		
+	fi	
+	mkdir $1
+else
+	echo "Please provide target folder"
+fi
+
+# Produce an executable (from here cannot be tested locally)
+
+module load Qt5
+qmake ../EGS.pro # assuming we are in the cluster folder
+make --silent release
+
+# Move the executable to the target folder
+
+mv EGS $1
+
+# Copy the protocol and the running script to the target folder
+
+cp protocol.txt $1
+cp run_simulations.sh $1
