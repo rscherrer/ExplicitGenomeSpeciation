a20361f15d43327b5019f981b816abf455ba7a97
rscherrer <raphael.scherrer@evobio.eu>
2020-01-29 14:06:12 +0100

    new argument to record data but not save them to files

--- library/Param.cpp
+++ library/Param.cpp
@@ -71,6 +71,7 @@ void Param::import(std::ifstream &file)
         else if (input == "tend") file >> tend;
         else if (input == "tsave") file >> tsave;
         else if (input == "record") file >> record;
+        else if (input == "datsave") file >> datsave;
         else if (input == "talkative") file >> talkative;
         else if (input == "archsave") file >> archsave;
         else if (input == "archload") file >> archload;
@@ -252,6 +253,7 @@ void Param::write(std::ofstream &file) const
     file << "tend " << tend << '\n';
     file << "tsave " << tsave << '\n';
     file << "record " << record << '\n';
+    file << "datsave " << datsave << '\n';
     file << "talkative " << talkative << '\n';
     file << "archsave " << archsave << '\n';
     file << "archload " << archload << '\n';
