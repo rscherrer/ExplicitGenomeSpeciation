ce99731c0b03485dbb38160628fa2726c0a98671
rscherrer <raphael.scherrer@evobio.eu>
2020-04-21 09:10:16 +0200

    functions to plot heatmaps for single simulations

--- egssimtools/test.R
+++ egssimtools/test.R
@@ -31,41 +31,24 @@ read_means(simulation)
 read_ecotype_means(simulation)
 read_ecotype_sizes(simulation)
 
-# Plot a simulation through time
-data <- read_individuals(simulation, "individual_trait")
-data <- lapply(data, function(data) sapply(data, "[", 1))
-data <- data.frame(time = mrep(as.numeric(names(data)), sapply(data, length)), data = do.call("c", data))
-
-p <- ggplot(data, aes(x = time, y = data)) +
-  geom_bin2d(bins = 15) +
-  theme_bw() +
-  scale_fill_continuous(type = "viridis") +
-  xlab("Time (generations)") +
-  ylab("Ecological trait") +
-  labs(fill = "Count")
+# Single plots
+p <- plot_traits_heatmap(simulation, trait = 1) +
+  ylab("Ecological trait")
 p
 
-matepref <- read_individuals(simulation, "individual_trait")
-matepref <- do.call("c", lapply(matepref, function(data) sapply(data, "[", 2)))
-data$matepref <- matepref
-
-p2 <- ggplot(data %>% filter(time == 2900), aes(x = matepref, y = data)) +
-  geom_bin2d(bins = 15) +
-  theme_bw() +
-  scale_fill_continuous(type = "viridis") +
-  xlab("Mate preference trait") +
+p2 <- plot_trait_heatmap2D(simulation, traits = c(2, 1), t = 2900) +
+  xlab("Mating preference") +
   ylab("Ecological trait") +
-  labs(fill = "Count") +
   xlim(c(-3, 3))
 p2
 
-ylim <- c(-3, 3)
-p <- p + theme(legend.position = "none") + ylim(ylim)
-p2 <- p2 + ylab(NULL) + ylim(ylim)
+# Rearrange into a single figure
+p <- p + theme(legend.position = "none") + ylim(c(-3, 3))
+p2 <- p2 + ylab(NULL) + ylim(c(-3, 3))
 
-p2 <- plot_grid(ggplot() + geom_blank() + theme_void(), p2, rel_widths = c(1, 11))
+p2. <- plot_grid(ggplot() + geom_blank() + theme_void(), p2, rel_widths = c(1, 11))
 
-p3 <- plot_grid(p, p2, labels = c("A", "B"), rel_widths = c(1, 1))
+p3 <- plot_grid(p, p2., labels = c("A", "B"), rel_widths = c(1, 1))
 p3
 
 ggsave("../pics/example_speciation.png", p3, height = 2, width = 6, dpi = 300)
