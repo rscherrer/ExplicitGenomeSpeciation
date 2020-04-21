92b7a9b64f3137e58b8989a88202d74e99603524
rscherrer <raphael.scherrer@evobio.eu>
2020-04-21 17:19:29 +0200

    working on big simulation plotting function

---
+++ egssimtools/R/plot_simulations.R
@@ -0,0 +1,143 @@
+#' Plot multiple simulations through time
+#'
+#' This function allows to easily loop through many simulation folders, extract data from them and plot them synthetically. Some population-wide variables are plotted through time for each simulation.
+#'
+#' @param root Where the simulation folders are
+#' @param variable What variable to plot
+#' @param pattern Optional pattern characteristic of simulation folders. Defaults to starting with "sim_".
+#' @param color Color of the lines. Defaults to black.
+#' @param pb Whether to display progress bars
+#' @param verbose Whether to display messages
+#' @param facet_rows Optional parameter name(s) to facet the plot by rows
+#' @param facet_cols Optional parameter name(s) to facet the plot by columns
+#' @param facet_wrapped Whether to automatically fill the facets by row with one-dimensional array of parameter combinations. If TRUE, combines the parameters in facet_rows and facet_cols into a single array. Ignored if none of those is specified.
+#' @param reverse_order Optional name of a parameter for which to reverse the order of the facets (e.g. to put highest values on the top rows)
+#' @param label_facets Whether to add custom labels to the facets
+#' @param facet_prefixes Optional prefixes to add to the facet labels of each parameter. Must have one value per facetting parameter. Ignored if label_facets is FALSE. If not specified and label_facets is TRUE, the names of the parameters are used as prefixes.
+#' @param sep Optional separator to use when adding prefixes. Defaults to " = ".
+#'
+#' @return A plot showing a variable through time for multiple simulations.
+#'
+#' @note The lines have different transparency levels, all very close to 0.5. This is a hack to make sure that all simulations can efficiently be plotted together on the same plot.
+#'
+#' @export
+
+plot_simulations <- function(
+  root,
+  variable,
+  pattern = "^sim_",
+  color = "black",
+  pb = TRUE,
+  verbose = TRUE,
+  facet_rows = NULL,
+  facet_cols = NULL,
+  facet_wrapped = FALSE,
+  reverse_order = NULL,
+  label_facets = FALSE,
+  facet_prefixes = NULL,
+  sep = " = ",
+  color_by = NULL,
+  color_by_numeric = TRUE
+) {
+
+  library(tidyverse)
+  library(pbapply)
+
+  if (!verbose) pb <- FALSE
+  if (pb) thislapply <- pblapply else thislapply <- lapply
+
+  # Look for missing and extinct simulations
+  if (verbose) message("Looking for missing simulations...")
+  missings <- find_missing(root, pattern = pattern, pb = pb)
+  if (verbose) message("Looking for extinct simulations...")
+  extincts <- find_extinct(root, pattern = pattern, pb = pb)
+
+  # Identify extant simulations
+  simulations <- list.files(root, pattern = pattern, full.names = TRUE)
+  if (!is.null(missings)) simulations <- simulations[!simulations %in% missings]
+  if (!is.null(extincts)) simulations <- simulations[!simulations %in% extincts]
+
+  # Collect the variable of interest from each simulation
+  if (verbose) message("Reading the data...")
+  data <- data.frame(do.call("rbind", thislapply(simulations, read_population, variable)))
+
+  # Add time if available
+  colnames(data) <- read_time_if(simulations[1])
+  timecolumns <- colnames(data)
+
+  # Add simulation identifier
+  data$simulation <- factor(rownames(data))
+
+  parnames <- c(facet_rows, facet_cols, color_by)
+
+  # Add parameter values if needed
+  if (!is.null(parnames)) {
+
+    if (verbose) message("Reading parameter values...")
+    parameters <- thislapply(simulations, read_parameters, parnames)
+
+    # Convert parameter values into factors in a data frame
+    parameters <- lapply(parameters, function(parameters) sapply(parameters, function(parameter) paste0(parameter, collapse = " ")))
+    parameters <- data.frame(do.call("rbind", parameters))
+    rownames(parameters) <- NULL
+
+    # Append to the data
+    data <- cbind(data, parameters)
+
+  }
+
+  # Convert from wide to long
+  data <- data %>% gather_("time", variable, timecolumns, convert = TRUE)
+
+  ### Choice 1: plot multiple lines ###
+
+  if (!is.null(reverse_order)) {
+    if (!all(reverse_order %in% parnames)) stop("Factor to reverse was not provided")
+    data[, reverse_order] <- lapply(data.frame(data[, reverse_order]), function(column) factor(column, levels = rev(levels(column))))
+  }
+
+  if (verbose) message("Plotting...")
+
+  # Show variable through time
+  p <- ggplot(data, aes(x = time, y = get(variable), alpha = simulation))
+
+  # Deal with color
+  if (!is.null(color_by)) {
+
+    # Color according to a parameter
+    if (color_by_numeric) thisconvert <- function(x) as.numeric(as.character(x)) else thisconvert <- factor
+    p <- p + geom_line(aes(color = thisconvert(get(color_by)))) +
+      labs(color = color_by)
+
+  } else p <- p + geom_line(color = color)
+
+  p <- p +
+    theme_bw() +
+    ylab(variable) +
+    guides(alpha = FALSE) +
+    scale_alpha_manual(values = runif(min = 0.49, max = 0.51, n = nlevels(data$simulation))) # this is a hack to show multiple lines on the same plot
+
+  facets <- c(facet_rows, facet_cols)
+
+  # Use facets if needed
+  if (!is.null(facets)) {
+
+    # Setup rows and columns
+    if (!is.null(facet_rows)) lhs <- paste(facet_rows, collapse = " + ") else lhs <- "."
+    if (!is.null(facet_cols)) rhs <- paste(facet_cols, collapse = " + ") else rhs <- "."
+
+    # Choose what type of facetting to use
+    if (facet_wrapped) thisfacet <- facet_wrap else thisfacet <- facet_grid
+
+    # Add custom labels if needed
+    if (is.null(facet_prefixes)) facet_prefixes <- facets
+    labels <- make_facet_labels(data, facets, facet_prefixes, sep = sep, no_use = !label_facets, simplify = FALSE)
+
+    # Split the plot into facets
+    p <- p + thisfacet(formula(paste(lhs, "~", rhs)), labeller = do.call("labeller", labels))
+
+  }
+
+  p
+
+}
--- egssimtools/test.R
+++ egssimtools/test.R
@@ -7,6 +7,8 @@ library(tidyverse)
 
 simulation <- "../build/release"
 
+# Low-level functions: work on single simulations
+
 read_paramfile(paste0(simulation, "/paramlog.txt"))
 read_parameters(simulation)
 read_binary(paste0(simulation, "/time.dat"))
@@ -23,9 +25,27 @@ read_ecotypes(simulation)
 read_habitats(simulation)
 read_loci(simulation, "genome_Fst")
 read_edges(simulation, "network_corgen")
-find_extinct("../build", pattern = "release")
-find_missing("../build", pattern = "release")
 read_resources(simulation)
 read_means(simulation)
 read_ecotype_means(simulation)
 read_ecotype_sizes(simulation)
+plot_trait_density(simulation, trait = 1)
+plot_trait_density2D(simulation, traits = c(2, 1), t = 100)
+
+
+# Medium-level functions: can combine and process data from different simulations but the output needs to be processed to produce full-fledged figures and results
+
+root <- "/media/raphael/bigass/simulations/EGS/EGS_sim1"
+
+find_extinct(root, pattern = "sim_")
+find_missing(root, pattern = "sim_")
+
+
+# High-level functions: produce figures and overall results over multiple simulations
+
+root <- "/media/raphael/bigass/simulations/EGS/EGS_sim1"
+
+plot_simulations(root, "RI", color_by = "hsymmetry", color = "darkblue", facet_rows = "ecosel", facet_cols = "hsymmetry", label_facets = TRUE, facet_prefixes = c("s", "h"), reverse_order = "ecosel", verbose = TRUE, pb = FALSE)
+plot_simulations(root, "EI", facet_rows = "hsymmetry", facet_wrapped = TRUE, color_by = "ecosel", label_facets = TRUE, facet_prefixes = "s", verbose = TRUE, pb = FALSE)
+plot_simulations(root, "SI", color = "darkorange", facet_rows = "ecosel", facet_cols = "hsymmetry", label_facets = TRUE, facet_prefixes = c("s", "h"), reverse_order = "ecosel", verbose = TRUE, pb = FALSE)
+
