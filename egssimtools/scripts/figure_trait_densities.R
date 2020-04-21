94a4277b34c334a0ff98078d5327c3acb3544aa2
rscherrer <raphael.scherrer@evobio.eu>
2020-04-21 09:37:43 +0200

    separate folder for scripts using the functions to make specific figures

---
+++ egssimtools/scripts/figure_trait_densities.R
@@ -0,0 +1,32 @@
+# Plot trait densities throughout the simulation
+
+rm(list = ls())
+
+library(egssimtools)
+library(tidyverse)
+library(cowplot)
+
+simulation <- "../build/release"
+
+# Ecological trait through time
+p <- plot_trait_density(simulation, trait = 1) +
+  ylab("Ecological trait")
+
+# Ecological versus mating preference traits at the last generation
+p2 <- plot_trait_density2D(simulation, traits = c(2, 1), t = 2900) +
+  xlab("Mating preference") +
+  ylab("Ecological trait") +
+  xlim(c(-3, 3))
+
+# Rearrange into a single figure
+p <- p + theme(legend.position = "none") + ylim(c(-3, 3))
+p2 <- p2 + ylab(NULL) + ylim(c(-3, 3))
+
+# Blank plot to fill up space
+p2. <- plot_grid(ggplot() + geom_blank() + theme_void(), p2, rel_widths = c(1, 11))
+
+# Assemble the plots
+p3 <- plot_grid(p, p2., labels = c("A", "B"), rel_widths = c(1, 1))
+p3
+
+ggsave("../pics/example_speciation.png", p3, height = 2, width = 6, dpi = 300)
--- egssimtools/test.R
+++ egssimtools/test.R
@@ -4,7 +4,6 @@ rm(list = ls())
 
 library(egssimtools)
 library(tidyverse)
-library(cowplot)
 
 simulation <- "../build/release"
 
@@ -30,25 +29,3 @@ read_resources(simulation)
 read_means(simulation)
 read_ecotype_means(simulation)
 read_ecotype_sizes(simulation)
-
-# Single plots
-p <- plot_trait_density(simulation, trait = 1) +
-  ylab("Ecological trait")
-p
-
-p2 <- plot_trait_density2D(simulation, traits = c(2, 1), t = 2900) +
-  xlab("Mating preference") +
-  ylab("Ecological trait") +
-  xlim(c(-3, 3))
-p2
-
-# Rearrange into a single figure
-p <- p + theme(legend.position = "none") + ylim(c(-3, 3))
-p2 <- p2 + ylab(NULL) + ylim(c(-3, 3))
-
-p2. <- plot_grid(ggplot() + geom_blank() + theme_void(), p2, rel_widths = c(1, 11))
-
-p3 <- plot_grid(p, p2., labels = c("A", "B"), rel_widths = c(1, 1))
-p3
-
-ggsave("../pics/example_speciation.png", p3, height = 2, width = 6, dpi = 300)
