8c9f394b5c0eaec82b14ae36e1923e8742b94515
rscherrer <raphael.scherrer@evobio.eu>
2020-05-20 13:02:59 +0200

    architecture fetching from collect sims

--- egssimtools/R/dplot_genome_heatmap.R
+++ egssimtools/R/dplot_genome_heatmap.R
@@ -4,12 +4,13 @@
 #'
 #' @param root Path to the simulation folder
 #' @param y Name of the variable to plot.
+#' @param archfile The architecture file to look for. Defaults to "architecture.txt"
 #'
 #' @return A ggplot
 #'
 #' @export
 
-dplot_genome_heatmap <- function(root, y) {
+dplot_genome_heatmap <- function(root, y, archfile = "architecture.txt") {
 
   library(ggplot2)
 
@@ -17,7 +18,11 @@ dplot_genome_heatmap <- function(root, y) {
   X <- read_data(root, y)
   nloci <- nrow(X) / nrow(time)
 
-  data <- read_data(root, c("time", y), dupl = c(nloci, 1), architecture = TRUE)
+  data <- read_data(
+    root, c("time", y), dupl = c(nloci, 1),
+    architecture = TRUE,
+    archfile = archfile
+  )
 
   ggplot(data, aes(x = time, y = locus, fill = get(y))) +
     geom_tile() +
