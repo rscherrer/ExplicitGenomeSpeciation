396fb0726ffe3fa3ae71989d584ec69830ef3c7d
rscherrer <raphael.scherrer@evobio.eu>
2020-11-06 15:25:55 +0100

    can specify the number of bins when plotting trait distributions across the population

--- egssimtools/R/plot_trait_values.R
+++ egssimtools/R/plot_trait_values.R
@@ -4,12 +4,13 @@
 #' Density heatmap of the three trait values across the population.
 #'
 #' @param data An individual-wise dataset where each trait is a different column.
+#' @param bins Number of bins to pass to `ggplot2::geom_bin2d`. Defaults to 30.
 #'
 #' @return A ggplot
 #'
 #' @export
 
-plot_trait_values <- function(data) {
+plot_trait_values <- function(data, bins = 30) {
 
   data <- tidyr::pivot_longer(data, cols = paste0("individual_trait", 1:3), names_to = "trait")
   data <- data %>% dplyr::mutate(trait = stringr::str_remove(trait, "individual_trait"))
@@ -17,7 +18,7 @@ plot_trait_values <- function(data) {
   ggsim::facettize(
 
     ggplot2::ggplot(data, ggplot2::aes(x = time, y = value)) +
-      ggplot2::geom_bin2d() +
+      ggplot2::geom_bin2d(bins = bins) +
       ggplot2::scale_fill_continuous(type = "viridis") +
       ggplot2::labs(x = "Time (generations)", y = "Trait value", fill = "Count"),
 
